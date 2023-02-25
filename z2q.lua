f323bcf468c207b78dc51fb2a3651c238b45f329
KimWoramet <101935471+KimWoramet@users.noreply.github.com>
2023-02-25 22:16:21 +0700

    Create z2q.lua

---
+++ z2q.lua
@@ -0,0 +1,7 @@
+repeat wait() until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+https://raw.githubusercontent.com/KimWoramet/script/main/bf.lua
+  else
+  game.Players.LocalPlayer:Kick("เข้าผิดแมพไอโง่")
+ end
